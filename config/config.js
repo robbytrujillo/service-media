5947f63c028588cf5af866b709d6af57d10fe4c9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-12 03:15:24 +0700

    Service-Media

---
+++ config/config.js
@@ -0,0 +1,24 @@
+// merubah file json menjadi js karena ingin memasukkan yang ada pada file .env
+module.export = {
+  development: {
+    username: "root",
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
