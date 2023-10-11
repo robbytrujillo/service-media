4e0dd75316044afe8343d2f17f37c6c591db363a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-12 03:19:44 +0700

    Service-Media

--- config/config.js
+++ config/config.js
@@ -10,22 +10,22 @@ const {
 
 module.export = {
   development: {
-    username: "root",
-    password: null,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
     database: "database_development",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql",
