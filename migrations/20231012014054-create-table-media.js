777b2324d8190a6a64680efea852376207cee8ba
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-12 12:31:52 +0700

    Service-Media | Migration & Model

--- migrations/20231012014054-create-table-media.js
+++ migrations/20231012014054-create-table-media.js
@@ -3,25 +3,25 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("media", 
-    { id: {
-      type: Sequelize.INTEGER, 
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-     },
-     image: {
-      type: Sequelize.STRING(250),
-      allowNull: false
-     },
-     created_at: {
-      type: Sequelize.DATE,
-      allowNull: false
-     },
-     update_at: {
-      type: Sequelize.DATE,
-      allowNull: false
-     }
+    return queryInterface.createTable("media", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING(250),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
